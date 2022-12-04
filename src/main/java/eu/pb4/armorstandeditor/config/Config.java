db8907413283b8489a275105fad4baddf6594fb0
DrexHD <nicknamedrex@gmail.com>
2022-12-04 22:02:39 +0100

    update: 1.19.3-rc1

--- src/main/java/eu/pb4/armorstandeditor/config/Config.java
+++ src/main/java/eu/pb4/armorstandeditor/config/Config.java
@@ -1,8 +1,8 @@
 package eu.pb4.armorstandeditor.config;
 
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registries;
 
 
 public class Config {
